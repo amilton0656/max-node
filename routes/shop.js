a7f17ec96e3f2e59c658ec173ca7bd1c1991dbfa
amilton0656 <amilton0656@gmail.com>
2022-02-04 17:02:51 -0300

    2022 02 04

--- routes/shop.js
+++ routes/shop.js
@@ -2,15 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const adminData = require('./admin');
-
-router.get('/', (req, res, next) => {
-    console.log('xxx', adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
